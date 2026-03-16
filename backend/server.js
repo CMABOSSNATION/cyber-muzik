d29b59d6f4ed8885a968d0051bd6f854a9d54f07
CMABOSSNATION <cmaraphael90@gmail.com>
2026-03-16 11:31:14 +0300

    Change server port from 5000 to 10000

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ if (!dbURI) {
 }
 
 // 5. Start Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`📡 Server listening on port ${PORT}`);
 });
